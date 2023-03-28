88da3d8f64aec5ff2c3a9e3275f5ea2ab3c25687
Brian Nkhata <bnkhata@mra.mw>
2023-03-28 09:58:48 +0200

    finished chat function

--- client/script.js
+++ client/script.js
@@ -37,13 +37,19 @@ function generateUniqueId() {
 }
 
 function chatStripe(isAi, value, uniqueId) {
-  return `
+  return (
+    `
     <div class="wrapper ${isAi && "ai"}">
       <div class="chat">
         <div class="profile">
-          <img src="${isAi ? bot : user}"/>
+          <img 
+          src="${isAi ? bot : user}"
+          alt="${isAi ? 'bot': 'user'}"
+          />
         </div>
+        <div class="message" id="${uniqueId}">${value}</div>
       </div>
     </div>
-    `;
+    `
+  );
 }
